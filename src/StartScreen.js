d70e5586cba1376a3ed5f5838ef520db922af1fa
Adelina <yadelishka@gmail.com>
2025-07-06 19:18:02 +0400

    add quesion component

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,16 @@
-function StartScreen({ numQuestions }) {
+import { type } from "@testing-library/user-event/dist/type";
+
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
-      <button className="btn btn-ui">Let's start</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's start
+      </button>
     </div>
   );
 }
